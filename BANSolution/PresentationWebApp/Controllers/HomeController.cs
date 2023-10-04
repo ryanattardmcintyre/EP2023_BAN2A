158561c23c125806b6992ce0df9aad884d821e45
Ryan Attard <ryanattard@gmail.com>
2023-10-04 12:26:34 +0200

    first commit - created an mvc application

---
+++ BANSolution/PresentationWebApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PresentationWebApp.Models;
+using System.Diagnostics;
+
+namespace PresentationWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //note: mvc works in such a way that it tries to find a View/page bearing the same name of the action e.g. Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
