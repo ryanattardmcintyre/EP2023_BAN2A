2ac7ac4f2bdcfbf040667e833463fad100f2a0ed
Ryan Attard <ryanattard@gmail.com>
2023-10-18 12:03:02 +0200

    Search Started the create

--- BANSolution/PresentationWebApp/Controllers/ProductsController.cs
+++ BANSolution/PresentationWebApp/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Repositories;
+using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using PresentationWebApp.Models.ViewModels;
 
@@ -33,6 +34,49 @@ namespace PresentationWebApp.Controllers
             return View(result);
         }
 
+        public IActionResult Search(string keyword) {
+            
+            //Select * From Products
+
+            var list = _productsRepository.GetProducts()
+                .Where(x=>x.Name.StartsWith(keyword) || x.Description.Contains(keyword))
+                .OrderBy(x => x.Name).ToList(); //there will be ONE database call
+
+            //transfer from Product >>>>> ProductViewModel
+            var result = from p in list
+                         select new ListProductsViewModel()
+                         {
+                             Name = p.Name,
+                             Description = p.Description,
+                             Id = p.Id,
+                             Image = p.Image,
+                             Price = p.Price,
+                             Stock = p.Stock,
+                             Category = p.Category.Name //using the navigational property
+                         };
+
+
+            return View("Index",result); //opening the Index View but passing a filtered list
+
+        }
+
+
+        //1. runs first and loads the page with empty fields to the user
+        [HttpGet]
+        public IActionResult Create() {
+            CreateProductViewModel myModel = new CreateProductViewModel();
+            //populate the Categories list from the db
+
+            
+            return View(); 
+        
+        }
+
+        //.....user inputs the details
+
+        //2. runs secondly with the parameters populated with the data....it saves into the db
+        [HttpPost]
+        public IActionResult Create(Product p) { }
         
     }
 }
---
+++ BANSolution/PresentationWebApp/Models/ViewModels/CreateProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PresentationWebApp.Models.ViewModels
+{
+    public class CreateProductViewModel
+    {
+        public List<Category> Categories { get; set; } //for display purposes 
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+
+        [Required]
+        public int CategoryFK { get; set; } //foreign key property: holds the value
+
+        public string Supplier { get; set; }
+        public double WholesalePrice { get; set; }
+
+        public string? Image { get; set; }
+    }
+}
